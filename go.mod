da379cf571974cb613a4e75c74df3dbc767851ed
Haris <mharis830174@gmail.com>
2023-07-07 20:06:06 +0700

    add parameter in say hello

--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
-module github.com/ritsuhaaa/go-say-hello
+module github.com/ritsuhaaa/go-say-hello/v2
 
 go 1.20
+
--- say-hello.go
+++ say-hello.go
@@ -1,5 +1,5 @@
 package gosayhello
 
-func SayHello()string{
-	return "Hello World, I'm Haris !"
+func SayHello(name string)string{
+	return "Hello" + name
 }
